6a6bd01d7a3614cd87cf691db6380fcd33e44f68
HugoKoot <Hugokoot1@gmail.com>
2020-09-29 19:05:19 +0200

    Updates

---
+++ precache-manifest.174d55660a9df709348c6d6c150363d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e269e37bad9e12d628f058f43721b3ca",
+    "url": "/react-drankspel/index.html"
+  },
+  {
+    "revision": "2289caccdb4a9def7a0d",
+    "url": "/react-drankspel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d4eb9d4fe7690838ed7",
+    "url": "/react-drankspel/static/js/2.bff715b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-drankspel/static/js/2.bff715b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2289caccdb4a9def7a0d",
+    "url": "/react-drankspel/static/js/main.26198348.chunk.js"
+  },
+  {
+    "revision": "27013f8e6860dd283c52",
+    "url": "/react-drankspel/static/js/runtime-main.b1e3fd1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-drankspel/static/media/logo.5d5d9eef.svg"
+  }
+]);
